d24f6ad9b8670050bb902e6a61dd9d402f7fdc8c
jakobmercer19 <>
2025-11-18 00:36:56 +0000

    Variable Naming

---
+++ variables_and_types/variable_naming/main.py
@@ -0,0 +1,11 @@
+#food name
+item_name = "Bread"
+#price_of_food
+item_price = 4.52
+#quantity
+item_quantity = 230
+
+# Testing
+print("Item:", item_name)
+print("Price per loaf: $", item_price)
+print("Quantity in stock:", item_quantity)
